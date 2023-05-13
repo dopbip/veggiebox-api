ff856350cd1562c13dc7bd74eee5db1a4042f5e5
dopbip <dopbipdev@gmail.com>
2023-05-13 15:31:45 +0200

    Update fruits.js

--- db/fruits.js
+++ db/fruits.js
@@ -3,11 +3,11 @@ const Schema = mongoose.Schema;
 
 const fruitModel = new Schema({
   name: { type: String, default: ''},
-  pack_price: { type: String, default: '' },
-  packed_items: { type: String, default: '' },
-  qty_in_store: { type: String, default: '' },
-  emoji: { type: String, default: '' },
-  key_word: { type: Array, default: [] },
+  pack_price: { type: Number, },
+  packed_items: { type: Number, },
+  qty_in_store: { type: Number},
+  emoji: { type: String,  },
+  key_word: { type: Array },
 
 });
 
