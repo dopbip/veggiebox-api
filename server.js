cb4c8db711d152251e23935b5ed9a6300deeae1a
dopbip <dopbipdev@gmail.com>
2023-05-16 12:08:29 +0200

    Update server.js

--- server.js
+++ server.js
@@ -70,6 +70,7 @@ const { parseInt } = require('lodash');
 
   app.post('/api/products/price/fruit_category', async(req,res) => {
     let { data } = req.body
+    console.log(data)
         let replyMsg = ``
         for (let i = 0; i < data.length; i++) {
           const element = data[i];
