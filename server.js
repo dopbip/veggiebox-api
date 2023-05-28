a9ad0059bbb03a81b27992dbabe672555370f92c
dopbip <dopbipdev@gmail.com>
2023-05-28 07:25:16 +0200

    Update server.js

--- server.js
+++ server.js
@@ -349,7 +349,7 @@ app.post('/api/add_delivery_user',
           length: 4,
           charset: 'numeric'
         })
-        let pin = hashPassword(randString)
+        let pin = await hashPassword(randString)
         console.log(pin)
         // create user
         let userData = await users.create({
